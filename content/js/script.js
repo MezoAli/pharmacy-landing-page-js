3ab07798c0652cc7d30e827832195804e0fc75bf
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-04-22 18:02:02 +0200

    adding project details

---
+++ content/js/script.js
@@ -0,0 +1,8 @@
+// adding loading spinner
+
+const body = window.document.body;
+
+window.addEventListener("load", function () {
+	body.classList.add("page-content-loaded");
+});
+console.log(window.document.documentElement);
